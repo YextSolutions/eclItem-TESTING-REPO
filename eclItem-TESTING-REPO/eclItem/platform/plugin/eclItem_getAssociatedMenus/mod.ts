2d15bebb050f77b52382a15b0271b8374744fe24
sreardonyext <100706230+sreardonyext@users.noreply.github.com>
2023-05-18 15:14:11 -0400

    Add files via upload

---
+++ eclItem-TESTING-REPO/eclItem/platform/plugin/eclItem_getAssociatedMenus/mod.ts
@@ -0,0 +1,44 @@
+//This function takes in a menu item name as an argument and returns an object with duplicate items and the names of the menus that contain them
+export async function getMenuArray(menuItemName) {
+
+
+  // Initialize offset and API endpoint URL for initial call
+  let offset = 0;
+  let totalMenusCount = 1;
+
+  // Initialize empty array to store duplicate items and associated menu names
+  const menuArray = [];
+
+  // Make initial API call to retrieve first page of results and convert response to JSON format
+ 
+
+  // While there are still menus left to retrieve, continue making API calls
+  while (offset < totalMenusCount) {
+
+    let apiUrl = 'https://api.yextapis.com/v2/accounts/me/menus?v=20230324&api_key=${{apiKey}}&limit=50&offset=' + offset; //
+
+    let response = await fetch(apiUrl);
+    let jsonResponse = await response.json();
+     // Retrieve total count of menus returned by API
+    totalMenusCount = jsonResponse.response.count;
+    if(typeof jsonResponse.response.menus == 'undefined') {return;}
+    // Loop through menus returned by API response
+    else {
+    for (let menu of jsonResponse.response.menus) {
+      for (let section of menu.sections) {
+        for (let item of section.items) {
+          if(item.name === menuItemName) {
+            menuArray.push(menu.name)
+          }
+        }
+      // If more than one item with matching name is found, add them to menuArray array with associated menu id
+    }
+  }
+  }
+    // Update offset and API endpoint URL for next API call
+    offset = offset + 50; 
+  }
+  var uniqueMenuArray = [...new Set(menuArray)]; // removes duplicates
+  return uniqueMenuArray.toString()
+  // Return an object with the array of duplicate items and their associated menu names
+}
